6e52ef0f4b94091d863131e7e6f625ae3c345c45
francavf <franca.vf20@gmail.com>
2023-11-22 17:24:41 -0300

    primeiro commit

---
+++ 05_FirstsStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/GOVRN/EDUC/StartupTests.java
@@ -0,0 +1,13 @@
+package GOVRN.EDUC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
